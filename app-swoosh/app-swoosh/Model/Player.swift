6dcec62eca67d2296e0be5daa4f7084de44ce4ac
Ryan McFadden <rjmcfadd@gmail.com>
2017-10-27 11:27:34 -0400

    Finished lesson-09

---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Ryan McFadden on 10/27/17.
+//  Copyright © 2017 Ryan McFadden. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+}
